8e56ed51e4196edfc356c55e75dd8cfd05e0f633
Donnovan Wint <donnovanwint@gmail.com>
2021-06-12 11:08:15 -0400

    init commit

---
+++ single.php
@@ -0,0 +1,14 @@
+<style>
+body {
+  background: lightblue;
+}
+</style>
+
+<?php get_header(); ?>
+
+<a href="/">Go back</a>
+
+<h2><?php the_title(); ?></h2>
+<p><?php the_content(); ?></p>
+
+<?php get_footer(); ?>
